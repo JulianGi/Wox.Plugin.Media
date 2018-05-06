8894d1be53c77bb9cf19263ce61f6055a8a1cdaf
juliangi <julian.giesen@outlook.de>
2018-05-06 17:37:27 +0200

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Wox.Plugin;
+using System.Windows.Interop;
+using System.Windows.Forms;
+
+namespace Wox.Plugin.Media
+{
+    public class Plugin : IPlugin
+    {
+        [DllImport("User32.dll")]
+        public static extern void keybd_event(uint bVk, uint bScan, uint dwFlags, uint dwExtraInfo);
+        public void Init(PluginInitContext context)
+        { 
+        }
+
+        public List<Result> Query(Query query)
+        {
+
+            var queryString = query.Search.ToLower();
+            List<Result> results = new List<Result>();
+            var icon = "image.png";
+            Dictionary<string, Result> QueryResults = new Dictionary<string, Result>
+            {
+                {"pa", Result("Play/Pause", "Play/Pause the current track", icon, Action("pause"))},
+                {"pl", Result("Play/Pause", "Play/Pause the current track", icon, Action("pause"))},
+                {"ne", Result("Next", "Skip the current track", icon, Action("next"))},
+                {"pr", Result("Previous", "Go to the last track", icon, Action("previous"))},
+                {"mu", Result("Mute/Unmute", "Toggle mute", icon, Action("mute"))},
+                {"sk", Result("Skip", "Skip the current track", icon, Action("next"))}
+
+            };
+
+            foreach (var x in QueryResults)
+            {
+                if (queryString.StartsWith(x.Key))
+                    {
+                        results.Add(x.Value);
+                    }
+            }
+            return results;
+        }
+
+        private static Result Result(String title, String subtitle, String icon, Func<ActionContext, bool> action)
+        {
+            return new Result()
+            {
+                
+                Title = title,
+                SubTitle = subtitle,
+                IcoPath = icon,
+                Action = action
+            };
+        }
+
+        // The Action method is called after the user selects the item
+        private static Func<ActionContext, bool> Action(String text)
+        {
+            return e =>
+            {
+                switch (text)
+                {
+                    case ("pause"):
+                        keybd_event(0xB3, 0, 0x0001 | 0, 0);
+                        break;
+                    case ("next"):
+                        keybd_event(0xB0, 0, 0x0001 | 0, 0);
+                        break;
+                    case ("previous"):
+                        keybd_event(0xB1, 0, 0x0001 | 0, 0);
+                        break;
+                    case ("mute"):
+                        keybd_event(0xAD, 0, 0x0001 | 0, 0);
+                        break;
+                   
+                    default:
+                        break;
+                }
+                // return false to tell Wox don't hide query window, otherwise Wox will hide it automatically
+                return false;
+            };
+        }
+
+    }
+       
+}
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Wox.Plugin.Media")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wox.Plugin.Media")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("4982fdfb-c328-4438-9c8a-150b78b7ceda")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
